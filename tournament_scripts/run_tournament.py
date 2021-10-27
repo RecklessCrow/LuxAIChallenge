746789c4a6da40f7b69c0a2cb8fa299d01a3e766
CJ Paterno <jmcc2646@gmial.com>
2021-10-27 18:08:18 -0400

    tournament scripts

--- tournament_scripts/run_tournament.py
+++ tournament_scripts/run_tournament.py
@@ -1,6 +1,8 @@
 import os
 from glob import glob
 
+import main_maker
+
 agents = [filename for filename in glob(os.path.join("model_drivers", "*.py"))]
 
 command = f"lux-ai-2021 --rankSystem=\"trueskill\" --tournament {' '.join(agents)} --maxtime 100000"
